da44b462854246f38ae6dd2c3959ce21989ad35c
xFly.Dragon <husano896@gmail.com>
2020-08-21 09:45:20 +0800

    IShakeable touch events

--- IShakeable/main.js
+++ IShakeable/main.js
@@ -3,9 +3,21 @@ var IAdjustable = document.getElementById('IAdjustable');
 
 var sensitive = 0.0002
 
+var lastTouch = null;
+
 document.onmousemove = function (event) {
     IShakeable.currentTime += event.movementX * sensitive;
 }
+
+document.addEventListener("touchstart", (event)=>{
+	lastTouch = event.touches[0];
+}, false);
+
+document.addEventListener("touchmove", (event)=>{
+	const newTouch = event.touches[0];
+	IShakeable.currentTime += (newTouch.pageX - lastTouch.pageX) * sensitive;
+	lastTouch = newTouch;
+}, false);
 
 IAdjustable.onchange = function (event) {
     sensitive = Number(event.srcElement.value);
