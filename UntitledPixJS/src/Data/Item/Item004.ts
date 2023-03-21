bf8033c0bc30cc86f477640894eef13b567c227c
xFly_Dragon <husano896@gmail.com>
2023-03-21 11:37:04 +0800

    加入道具描述

---
+++ UntitledPixJS/src/Data/Item/Item004.ts
@@ -0,0 +1,8 @@
+import { IItem } from "./IItem";
+
+const Item: IItem = {
+    name: '淺水生魚',
+    description: '在河邊可捕捉到, 可生吃也可簡單調味就變得很好吃!'
+}
+
+export default Item
---
+++ UntitledPixJS/src/Data/Item/index.ts
@@ -0,0 +1,13 @@
+import Item001 from "./Item001";
+import Item002 from "./Item002";
+import Item003 from "./Item003";
+import Item004 from "./Item004";
+
+const Items = {
+    1: Item001,
+    2: Item002,
+    3: Item003,
+    4: Item004,
+}
+
+export default Items
